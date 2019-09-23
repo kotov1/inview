d195a77c5de6a377d09e21ae77531748ade71cbe
Petr Petukhov <kotov0991@gmail.com>
2019-09-23 12:48:05 +0300

    finished

--- app/js/common.js
+++ app/js/common.js
@@ -5,19 +5,8 @@ $(function() {
 		$(this).toggleClass("nav--expanded");
 	});
 	
-	// Выделение фиксированного меню при скролле страницы на адаптиве
-	// $(window).scroll(function(){
-	// 	if( $(window).width() < 992 ) {
-	// 		var headerHeight = $('.header').height();
-	// 		if ($(window).scrollTop() >= headerHeight) {
-	// 			$('.header').addClass('header--fixed');
-	// 		} else {
-	// 			$('.header').removeClass('header--fixed');
-	// 		}
-	// 	}
-	// });
 
-	
+	// Скрипт прокрутки к якорю
 	$(".anchor").click(function (event) {
 		event.preventDefault();
 		var id  = $(this).attr('href'),
@@ -26,8 +15,7 @@ $(function() {
 	});
 
 
-
-	// Ютуб видео
+	// Генерация атрибута href ссылкок на ютуб видео
 	$('.video').each(function (index, element) {
 		if( $(this).attr('data-video') ) {
 			var id = $(this).data('video');
@@ -36,7 +24,7 @@ $(function() {
 		}
 	});
 
-
+	// Вызов модального видео
 	$('.js-video').magnificPopup({
 		type: 'iframe',
 		mainClass: 'mfp-fade',
@@ -44,6 +32,7 @@ $(function() {
 	});
 
 
+	// Слайдер с примерами работ
 	$('.examples-slider').slick({
 		infinite: false,
 		slidesToShow: 3,
@@ -64,6 +53,48 @@ $(function() {
 	});
 
 
+	// Плейсхолдеры в инпутах
+	$('.input').on('change', function() {
+		var input = $(this);
+		if (input.val().length) {
+			input.addClass('populated');
+		} else {
+			input.removeClass('populated');
+		}
+	});
+
+
+	// Вызов модального окна
+	$('.js-call-modal').magnificPopup({
+		items: {
+			src: '#modal-callback',
+			type: 'inline'
+		}
+	});
+
+	// Simple dropdowns
+	$('.dropdown__link').on('click', function(e) {
+		e.preventDefault();
+		$(this).siblings('.dropdown__content').slideToggle('fast');
+	});
+
+
+	// Переключение табов тиммемберов по ховеру
+	$(".team-member").mouseenter(function() {
+		var index = $(this).index();
+		  $(this).closest('.team').find(".team-member").removeClass("team-member--hover").eq(index).addClass("team-member--hover");
+		//   $(this).closest('.team').find(".team-content").hide().eq(index).fadeIn("normal");
+	});
+
+	// минимальная высота блоку контента о тиммемберах, чтобы не было скачков
+	var teamContentHeight = 0;
+	$('.team-content').each(function () {
+		if( $(this).height() > teamContentHeight ) {
+			teamContentHeight = $(this).height();
+		}
+	});
+	$('.team').find('.block-min-height').css('min-height', teamContentHeight);
+	  
 
 
 
@@ -71,5 +102,10 @@ $(function() {
 
 
 
+
+
+
+
+	
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/slick-carousel/slick/slick.min.js',
 		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
-		'app/libs/wow.min.js',
+		// 'app/libs/wow.min.js',
 		'app/js/common.js',
 		])
 	.pipe(concat('scripts.js'))
